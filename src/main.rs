6998df9539a5b4e991eea423bbbff73c059e63b1
Andrew Eggleston <egglestonandrew927@gmail.com>
2018-10-22 16:23:10 -0400

    Test notify

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
+extern crate lettre;
+extern crate notify;
 
+use notify::{RecommendedWatcher, Watcher};
+use std::sync::mpsc::channel;
+use std::time::Duration;
+
+
+fn watch() -> notify::Result<()> {
+    let (tx, rx) = channel();
+
+    let mut watcher: RecommendedWatcher = try!(Watcher::new(tx, Duration::from_secs(1)));
+
+    try!(watcher.watch("/sys/devices/w1_bus_master1/w1_master_slaves"));
+
+    loop {
+        match rx.recv() {
+            Ok(event) => println!("{:?}", event),
+            Err(e) => println!("watch error: {:?}", e),
+        }
+    }
+}
+
+fn main() {
+    if let Err(e) = watch() {
+        println!("error: {:?}", e)
+    }
 }
