87bb56e486e2950c6aa4342345a822cd1d533793
root <egglestonandrew927@gmail.com>
2018-10-23 23:34:40 +0000

    Polling for iButton functioning

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ extern crate notify;
 use notify::{Watcher, RawEvent, PollWatcher};
 use std::sync::mpsc::channel;
 use std::time::Duration;
+use std::fs::File;
+use std::io::prelude::*;
 use notify::RecursiveMode;
 
 fn watch() -> notify::Result<()> {
@@ -13,16 +15,32 @@ fn watch() -> notify::Result<()> {
 
     println!("Started!");
 
-    watcher.watch("/sys/devices/w1_bus_master1", RecursiveMode::Recursive).unwrap();
+    watcher.watch("/sys/devices/w1_bus_master1", RecursiveMode::NonRecursive).unwrap();
 
     //try!(watcher.watch("/root", RecursiveMode::Recursive));
 
     loop {
         match rx.recv() {
-            Ok(event) => println!("{:?}", event),
+            Ok(event) => {
+			     runScan()
+			 },
 	    Err(e) => println!("watch error: {:?}", e),
         }
     }
+}
+
+fn runScan(){
+    let mut f = File::open("/sys/devices/w1_bus_master1/w1_master_slaves").unwrap();
+    let mut contents = String::new();
+    f.read_to_string(&mut contents).unwrap();
+    if contents.trim() != "not found."{    
+	getUsername(contents);
+    }
+}
+
+fn getUsername(username: String){
+    println!("Sending get for ibutton {}", username);    
+//send get request
 }
 
 fn main() {
