d69782abe3715e224d4af924c823d74d19b62f95
Andrew Eggleston <egglestonandrew927@gmail.com>
2018-10-24 23:05:56 -0400

    big teste 5

--- src/main.rs
+++ src/main.rs
@@ -86,7 +86,7 @@ fn send_email(username: String) {
     let mime = "image/jpeg".parse::<mime::Mime>().unwrap();
     emailbuilder.attachment(&Path::new("/scans/scan.jpg"), None, &mime);
     let mut email = &emailbuilder.build().expect("Should be a valid email");
-    let mut sendableemail: SendableEmail<Email> = email.into();
+    let mut sendableemail: SendableEmail<Email> = &email.into();
     let mut mailer: lettre::sendmail::SendmailTransport = SendmailTransport::new();
     mailer.send(sendableemail);
 }
